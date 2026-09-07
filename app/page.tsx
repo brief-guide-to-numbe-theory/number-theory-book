543c029d453f98a42966e580853f8478675baa11
saha2008aritra-beep <saha2008aritra@gmail.com>
2026-09-08 02:28:31 +0530

    s

--- app/page.tsx
+++ app/page.tsx
@@ -418,13 +418,13 @@ export default function Home() {
 
               <div className="acknowledgements-body">
                 <p>
-                  Firstly, both of us would like to thank our parents for supporting us throughout this journey. We would also like to express our sincere gratitude to Rushil Mathur, Malay Mahajan, Kanav Talwar, and Arindam Bhattacharya for their valuable advice, insightful suggestions, and continuous encouragement during the entire process.
+                  Firstly, both of us would like to thank our parents for supporting us throughout this journey. We would also like to express our sincere gratitude to <strong>Rushil Mathur</strong>, <strong>Malay Mahajan</strong>, <strong>Kanav Talwar</strong>, and <strong>Arindam Bhattacharya</strong> for their valuable advice, insightful suggestions, and continuous encouragement during the entire process.
                 </p>
                 <p>
-                  We are deeply thankful to our friend Mandar Kasulkar for writing certain parts of the book. We would also like to thank our friends Ronit Sharma, Bairav Murugan (IMO Silver-Medalist), Paras Kumar, Tanishka Gham, Lavish Khariwal and Nishant Sahoo for their generous assistance in reviewing the manuscript, proofchecking, identifying mathematical and grammatical errors, and offering numerous helpful comments.
+                  We are deeply thankful to our friend <strong>Mandar Kasulkar</strong> for writing certain parts of the book. We would also like to thank our friends <strong>Ronit Sharma</strong>, <strong>Bairav Murugan</strong> (IMO Silver-Medalist), <strong>Paras Kumar</strong>, <strong>Tanishka Gham</strong>, <strong>Lavish Khariwal</strong>, and <strong>Nishant Sahoo</strong> for their generous assistance in reviewing the manuscript, proofchecking, identifying mathematical and grammatical errors, and offering numerous helpful comments.
                 </p>
                 <p>
-                  Last, but certainly not least, we would like to thank Abhay Mahajan Sir, who has mentored both of us throughout our Olympiad journey and has also played the crucial role of helping us get in touch with publishers. Special thanks to Roumak Das.
+                  Last, but certainly not least, we would like to thank <strong>Abhay Mahajan Sir</strong>, who has mentored both of us throughout our Olympiad journey and has also played the crucial role of helping us get in touch with publishers. Special thanks to <strong>Roumak Das</strong>.
                 </p>
               </div>
             </div>
